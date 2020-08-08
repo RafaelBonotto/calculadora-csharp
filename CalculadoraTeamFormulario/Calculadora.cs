94c71171539d0cd56f7806808b17eb1b0e396da2
RafaelBonotto <rafareisaba@gmail.com>
2020-08-08 20:44:06 -0300

    Calculadora c#

---
+++ CalculadoraTeamFormulario/Calculadora.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculadoraTeamFormulario
+{
+    class Calculadora
+    {
+        public double resultado;
+        public Calculadora()
+        {
+            resultado = 0;
+        }//fim do construtor
+
+        //Consultar Resultado
+        public double ConsultarResultado
+        {
+            get
+            {
+                return resultado;
+            }
+            set
+            {
+                this.resultado = value;
+            }
+        }//fim do resultado
+
+        //Soma
+        public double Soma(double num1, double num2)
+        {
+            return num1 + num2;//Soma
+        }//fim do método 
+        public double Subtracao(double num1, double num2)
+        {
+            return num1 - num2;//Subtração
+        }//fim do método
+        public double Divisao(double num1, double num2)
+        {
+            if(num1 > 0 && num2 > 0)
+            {
+                return num1 / num2;
+            }
+            return -1;//retorno do método
+        }//fim do método
+        public double Multiplicacao(double num1, double num2)
+        {
+            return num1 * num2;
+        }//fim do método
+        public double Resto(double num1, double num2)
+        {
+            if (num1 > 0 && num2 > 0)
+            {
+                return num1 % num2;
+            }
+            return -1;
+        }//fim do método
+        public double Potencia(double num1, double num2)
+        {
+            return Math.Pow(num1, num2);//Método POW = Representa um potência na classe matemática
+        }//fim do método
+    }//fim da classe
+}//fim do projeto
---
+++ CalculadoraTeamFormulario/Form1.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CalculadoraTeamFormulario
+{
+    public partial class Form1 : Form
+    {
+        Calculadora calc;
+        public Form1()
+        {
+            InitializeComponent();
+            calc = new Calculadora();
+        }//Fim do construtor
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }//Carregar
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }//Campo para entrada do número 1
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }//Campo para entrada do número 2
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }//Campo para mostrar o resultado
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                textBox3.Text = "" + calc.Soma(Convert.ToDouble(textBox1.Text), Convert.ToDouble(textBox2.Text));
+            }catch(Exception erro)
+            {
+                MessageBox.Show("Um ou mais campos estão vazios");
+            }
+        }//Soma
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try { 
+                textBox3.Text = "" + calc.Subtracao(Convert.ToDouble(textBox1.Text), Convert.ToDouble(textBox2.Text));
+            }catch(Exception erro)
+            {
+                MessageBox.Show("Um ou mais campos estão vazios");
+            }
+        }//Subtração
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            try { 
+                textBox3.Text = "" + calc.Multiplicacao(Convert.ToDouble(textBox1.Text), Convert.ToDouble(textBox2.Text));
+            }catch(Exception erro)
+            {
+                MessageBox.Show("Um ou mais campos estão vazios");
+            }
+        }//Multiplicação
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            try { 
+                textBox3.Text = "" + calc.Divisao(Convert.ToDouble(textBox1.Text), Convert.ToDouble(textBox2.Text));
+            }catch(Exception erro)
+            {
+                MessageBox.Show("Um ou mais campos estão vazios");
+            }
+        }//Divisão
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            try { 
+                textBox3.Text = "" + calc.Resto(Convert.ToDouble(textBox1.Text), Convert.ToDouble(textBox2.Text));
+            }catch(Exception erro)
+            {
+                MessageBox.Show("Um ou mais campos estão vazios");
+            }
+        }//Resto
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            try { 
+                textBox3.Text = "" + calc.Potencia(Convert.ToDouble(textBox1.Text), Convert.ToDouble(textBox2.Text));
+            }catch(Exception erro)
+            {
+                MessageBox.Show("Um ou mais campos estão vazios");
+            }
+        }//Potência
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+            textBox2.Text = "";
+            textBox3.Text = "";
+        }//Limpar
+
+
+    }//fim da classe 
+}//Fim do projeto
